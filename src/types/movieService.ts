271b082f90a73b0436922103c2c094b7fe372731
Nunam-UK <ivanolef.io@gmail.com>
2025-12-21 18:00:07 +0200

    Fixed hw

--- src/types/movieService.ts
+++ src/types/movieService.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { Movie } from "../types/movie";
+import type { Movie } from "../types/movie";
 
 // 1. Отримуємо токен із змінної оточення
 const TOKEN = import.meta.env.VITE_TMDB_TOKEN;
